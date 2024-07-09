4beb43469918a45ec3c253b7d65f5d8c4365c5e7
Nicola-Lippo <nicolalippo8991@gmail.com>
2024-07-09 19:23:39 +0200

    [UPD] Rappresentazione dei dati in pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,28 @@
 </head>
 
 <body>
-    <h1>weeeeeeeee</h1>
+    <div class="container">
+        <h1 class="text-center">Lista Film Database</h1>
+        <section>
+            <div class="container">
+                <div class="d-flex">
+                    <ul class="row">
+                        @foreach ($movies as $movie)
+                            <li class="col-4 p-3">
+                                <div>TItolo: {{ $movie['title'] }}</div>
+                                <ul>
+                                    <li>Titolo originale: {{ $movie['original_title'] }}</li>
+                                    <li>Nazionalità: {{ $movie['nationality'] }}</li>
+                                    <li>Data Uscita: {{ $movie['date'] }}</li>
+                                    <li>Voto: {{ $movie['vote'] }}</li>
+                                </ul>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </section>
+    </div>
 </body>
 
 </html>
